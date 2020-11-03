8648783e3e53c641161f54052fd198b2056326d2
49596lokken <lars.okken@gmail.com>
2020-11-03 16:59:06 +0000

    improved the encode algorithm for easier use

--- Categories/useful.py
+++ Categories/useful.py
@@ -13,36 +13,53 @@ class useful(commands.Cog):
         self.all_characters = [chr(i) for i in range(32, 127)]
 
     
-    @commands.command(description="encodes a piece of text according to an algorithm. Encoding is the same as decoding",rest_is_raw=True)
-    async def encode(self, ctx, password, number: int, *,message):
-        message = message[1:]   
+    def cipher(self, target_sum, number, message):
         output = ""
-        target_sum = 0
+        for char in message:
+            target_sum %= len(self.all_characters)
+            if not char in message:
+                raise Exception(char=char)
+            new_char = self.all_characters[(target_sum-self.all_characters.index(char)%len(self.all_characters))]
+            output += new_char
+            target_sum += number
+        return(output)
+            
+
+
+    @commands.command(description="encodes a piece of text according to a specific password. Encoding is the same as decoding.\n To add spaces at the end, put a space and send a random character after. To add at the beginning, just have an extra space after the password",rest_is_raw=True)
+    async def encode(self, ctx, password, *,message):
+        if len(password) < 8:
+            await ctx.send("Your password has to be at least 8 characters long")
+        message = message[1:]   
         for char in password:
             if not char in self.all_characters:
                 await ctx.send(f"character: \"{char}\" not supported")
                 return
-            target_sum += self.all_characters.index(char)
-        for char in message:
-            target_sum %= len(self.all_characters)
-            if not char in self.all_characters:
-                await ctx.send(f"character: \"{char}\" not supported")
-                return
-            new_char = self.all_characters[(target_sum-self.all_characters.index(char)%len(self.all_characters))]
-            output += "\\"*int(not new_char.upper() in self.alphabet)+new_char
-            target_sum += number
-        if output[0] == " ":
+        number = sum(self.all_characters.index(i) for i in password[(len(password)%2)-2:])
+        password = password[:(len(password)%2)-2]
+        for i in range(len(password)//2):
+            target_sum = self.all_characters.index(password[2*i])+self.all_characters.index(password[2*i+1])
+            message = self.cipher(target_sum, number, message)
+        for i in range(2,len(password)//2+1):
+            target_sum = self.all_characters.index(password[-2*i])+self.all_characters.index(password[-2*i+1])
+            message = self.cipher(target_sum, number, message)
+        offset = 0
+        for i in range(len(message)):
+            if not message[i+offset].upper() in self.alphabet:
+                message = message[:i+offset]+"\\"+message[i+offset:]
+                offset += 1
+        if message[0] == " ":
             i = 1
-            while output[i] == " ":
+            while message[i] == " ":
                 i += 1
             await ctx.send(f"{i} space(s) at the start")
-        if output[-1] == " ":
+        if message[-1] == " ":
             i = 1
-            while output[len(output)-1-i] == " ":
+            while message[len(message)-1-i] == " ":
                 i += 1
             await ctx.send(f"{i} space(s) at the end")
 
-        await ctx.send(output)
+        await ctx.send(message)
     
     @commands.command(description="sends you an invite to the test server")
     async def test(self, ctx):
