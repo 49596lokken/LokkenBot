345d9ceb9d17d3b561737eb7970bd05b790efe21
49596lokken <lars.okken@gmail.com>
2020-11-20 17:21:49 +0000

    Fixed the ping for prefix being called in replies

--- main.py
+++ main.py
@@ -36,11 +36,13 @@ def findprefix(bot, message):
 bot = commands.Bot(command_prefix=findprefix, case_insensitive=True, help_command=None,intents=intents)
 
 @bot.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if len(message.mentions) == 1:
-        if message.mentions[0] == bot.user and message.content.index("<") == 0 and message.content.index(">") == len(message.content)-1 and not ("<" in message.content[1:]):
+        if message.mentions[0] == bot.user and not message.reference and message.content.index("<") == 0 and message.content.index(">") == len(message.content)-1 and not ("<" in message.content[1:]):
             await message.channel.send(f"The prefix is \"{findprefix(bot, message)}\"")
             return
+    if message.reference:
+        await message.channel.send((await message.channel.fetch_message(message.reference.message_id)).content)
     await bot.process_commands(message)
 
 
